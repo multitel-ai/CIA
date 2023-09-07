63a6c1d979c1f73015f3e18b2eebf22cdaebdc2c
hatellezp <hatellezp@gmail.com>
2023-09-07 09:13:07 +0200

    [ADD] adding documentation to the readme along with some (for the moment bad) examples.
    Also changed some code in the gen.py file to keep track of the base
    image used for generation and the featured extracted from it.

--- iqa.py
+++ iqa.py
@@ -1,6 +1,16 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
+# In this file the approach to measure quality will be the extensive library
+# IQA-Pytorch: https://github.com/chaofengc/IQA-PyTorch
+# Read also the paper: https://arxiv.org/pdf/2208.14818.pdf
+
+# There are basically two approaches to measure image quality
+# - full reference: compare againts a real pristine image
+# - no reference: compute metrics following a learned opinion
+
+# Because images are generated there is no reference image to compare to. We
+# will be using with the no-reference metrics
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg : DictConfig) -> None:
