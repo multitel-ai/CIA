08b8b082b325aa32e4267c89a6bd405899cb325f
hatellezp <hatellezp@gmail.com>
2023-09-11 09:49:48 +0200

    [REF] Cleaning the code and little refactoring
    - `draw_landmakrs_on_binary_image` was removed, both logics live in
      `draw_landmarks_on_image` with a `mode` argument to choose

--- common.py
+++ common.py
@@ -1,69 +1,32 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 import mediapipe as mp
 from mediapipe.framework.formats import landmark_pb2
 from mediapipe import solutions
+from PIL import Image
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 
-def find_model_name(name: str, l: List[Dict[str, str]]) -> str:
+
+def find_model_name(name: str, l: List[Dict[str, str]]) -> Optional[str]:
     for small_dict in l:
         if name in small_dict:
             return small_dict[name]
     return None
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
+def draw_landmarks_on_image(rgb_image, detection_result, mode:str = 'default'):
+    if mode not in ('default', 'binary'):
+        raise Exception(f"Unkown mode: {mode}")
+
     face_landmarks_list = detection_result.face_landmarks
+    if mode == 'binary':
+        rgb_image = Image.new("RGB", (rgb_image.shape[0], rgb_image.shape[1]), (0, 0, 0))
     annotated_image = np.copy(rgb_image)
 
     # Loop through the detected faces to visualize.
-    for idx in range(len(face_landmarks_list)):
-        face_landmarks = face_landmarks_list[idx]
-
-        # Draw the face landmarks.
-        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        face_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
-        ])
-
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles
-            .get_default_face_mesh_tesselation_style())
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles
-            .get_default_face_mesh_contours_style())
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_IRISES,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles
-            .get_default_face_mesh_iris_connections_style())
-
-    return annotated_image
-
-
-def draw_landmarks_on_binary_image(rgb_image, detection_result):
-    face_landmarks_list = detection_result.face_landmarks
-
-    rgb_image = Image.new("RGB", (rgb_image.shape[0], rgb_image.shape[1]), (0, 0, 0))
-    annotated_image = np.copy(rgb_image)
-
-    # Loop through the detected faces to visualize.
-    for idx in range(len(face_landmarks_list)):
-        face_landmarks = face_landmarks_list[idx]
-
+    for face_landmarks in face_landmarks_list:
         # Draw the face landmarks.
         face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         face_landmarks_proto.landmark.extend([
@@ -97,14 +60,15 @@ def draw_landmarks_on_binary_image(rgb_image, detection_result):
 
 def plot_face_blendshapes_bar_graph(face_blendshapes):
     # Extract the face blendshapes category names and scores.
-    face_blendshapes_names = [
-        face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
-    face_blendshapes_scores = [
-        face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
+    face_blendshapes_names, face_blendshapes_scores = [], []
+    for face_blendshapes_category in face_blendshapes:
+        face_blendshapes_names.append(face_blendshapes_category.category_name)
+        face_blendshapes_scores.append(face_blendshapes_category.category_score)
+
     # The blendshapes are ordered in decreasing score value.
     face_blendshapes_ranks = range(len(face_blendshapes_names))
 
-    fig, ax = plt.subplots(figsize=(12, 12))
+    _, ax = plt.subplots(figsize=(12, 12))
     bar = ax.barh(face_blendshapes_ranks, face_blendshapes_scores,
                   label=[str(x) for x in face_blendshapes_ranks])
     ax.set_yticks(face_blendshapes_ranks, face_blendshapes_names)
