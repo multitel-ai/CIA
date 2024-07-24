4090c8bc070137fb9b47ef88cc3b657c095b94b5
hatellezp <hatellezp@gmail.com>
2024-07-24 12:38:21 +0200

    [COM] putting comments for upgrade here and there

--- run.sh
+++ run.sh
@@ -19,6 +19,8 @@
 #   - iqa for measuring the quality of generated images
 #   - train
 
+
+# TODO: this should be a python file in the future.
 if [[ $1 == "gen" || ($1 == "coco" || ( $1 == "flickr30k" || ($1 == "iqa" || ($1 == "train" || ( $1 == "iqa_paper" || ( $1 == "create_dataset" || ( $1 == "download" || ( $1 == "test" || $1 == "create_n_train" ) ) ) ) ) )  ) ) ]]; then
     python3 "src/$1.py" ${@:2}
 else
